6c21cb5121b80d2e26655156e837afa766a0af6b
AnthonyCaicedo <anthonycaicedo8@gmail.com>
2017-01-11 01:05:50 -0500

    Mejora en el index de la aplicación

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,7 +10,7 @@ $this->title = 'Home';
         <h1>PUCESE</h1>
 
         <p class="lead">SisHNA</p>
-        <p class="lead">Bienvenidos al sistema de registro de horas no académicas</p>
+        <h6 class="lead">Bienvenidos al sistema  de registro de horas no académicas</h6>
 
   
     </div>
